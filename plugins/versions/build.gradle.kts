f5409329c72eb54e8510636b416c6af596ef07e3
daz <daz@gradle.com>
2024-02-11 23:03:44 -0700

    Fix versions

--- plugins/versions/build.gradle.kts
+++ plugins/versions/build.gradle.kts
@@ -1,3 +1,15 @@
+// Upgrade transitive dependencies in plugin classpath
+buildscript {
+    repositories {
+        gradlePluginPortal()
+    }
+    dependencies {
+        constraints {
+            classpath(libs.okio)
+        }
+    }
+}
+
 plugins {
     alias(libs.plugins.versions) apply(false)
 }
