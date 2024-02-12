9fbc3d6b3995cc953e9465051a428adde530e94a
daz <daz@gradle.com>
2024-02-11 23:08:16 -0700

    Fix spring-boot

--- plugins/spring-boot/build.gradle.kts
+++ plugins/spring-boot/build.gradle.kts
@@ -1,3 +1,15 @@
+// Upgrade transitive dependencies in plugin classpath
+buildscript {
+    repositories {
+        gradlePluginPortal()
+    }
+    dependencies {
+        constraints {
+            classpath(libs.commons.compress)
+        }
+    }
+}
+
 plugins {
     alias(libs.plugins.spring.boot) apply(false)
 }
