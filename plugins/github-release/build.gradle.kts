13fc579966ffa6cd936cc428a97fdd807343e5c7
daz <daz@gradle.com>
2024-02-11 21:39:02 -0700

    Fix github-release

--- plugins/github-release/build.gradle.kts
+++ plugins/github-release/build.gradle.kts
@@ -1,3 +1,15 @@
+// Upgrade transitive dependencies in plugin classpath
+buildscript {
+    repositories {
+        gradlePluginPortal()
+    }
+    dependencies {
+        constraints {
+            classpath("com.squareup.okio:okio:3.4.0")
+        }
+    }
+}
+
 plugins {
     alias(libs.plugins.github.release) apply(false)
 }
