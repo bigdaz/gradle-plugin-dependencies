ab5af2d94f8cc02773fc74e82f05a951ecccfd7a
daz <daz@gradle.com>
2024-02-12 20:21:41 -0700

    Upgrade jose4j

--- plugins/android/build.gradle.kts
+++ plugins/android/build.gradle.kts
@@ -6,6 +6,9 @@
 //      https://github.com/advisories/GHSA-6628-q6j9-w8vg
 //      https://github.com/advisories/GHSA-cfgp-2977-2fmm
 //      https://github.com/advisories/GHSA-9hxf-ppjv-w6rq
+// Override 'org.bitbucket.b_c:jose4j' to avoid:
+//      https://github.com/advisories/GHSA-7g24-qg88-p43q
+//      https://github.com/advisories/GHSA-jgvc-jfgh-rjvv
 buildscript {
     repositories {
         gradlePluginPortal()
@@ -13,6 +16,7 @@ buildscript {
     dependencies {
         constraints {
             classpath(libs.gprc.protobuf)
+            classpath(libs.jose4j)
         }
     }
 }
