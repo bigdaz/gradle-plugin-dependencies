2daf0d4eefd06ed5938f51d1aa171ec39e109e42
daz <daz@gradle.com>
2024-02-12 20:58:56 -0700

    Apply more android plugins

--- plugins/android/build.gradle.kts
+++ plugins/android/build.gradle.kts
@@ -32,9 +32,9 @@ buildscript {
 // Based on nowinandroid
 plugins {
     alias(libs.plugins.android.application) apply false
-    /*
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.test) apply false
+    /*
     alias(libs.plugins.baselineprofile) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
