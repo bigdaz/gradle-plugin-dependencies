67958af1f74c200e827ebcd6b1e3858ca14f934b
daz <daz@gradle.com>
2024-02-12 21:27:44 -0700

    Bump okhttp instead of okio

--- plugins/github-release/build.gradle.kts
+++ plugins/github-release/build.gradle.kts
@@ -1,12 +1,12 @@
 // FIX Dependabot Alert
-// Override 'com.squareup.okio:okio' to avoid https://github.com/advisories/GHSA-w33c-445m-f8w7
+// Override 'com.squareup.okhttp3:okhttp' to avoid https://github.com/advisories/GHSA-w33c-445m-f8w7
 buildscript {
     repositories {
         gradlePluginPortal()
     }
     dependencies {
         constraints {
-            classpath("com.squareup.okio:okio:3.4.0")
+            classpath("com.squareup.okhttp3:okhttp:4.12.0")
         }
     }
 }
